52b54b494530c46b463b3146d2ea4b55cdd62e09
sourav_allmysons <souravmajumdar.dev@gmail.com>
2023-06-24 10:03:39 +0530

    Complete ProductDetailsScreen page UI

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import { StyleSheet, View } from 'react-native';
 import ProductsScreen from './src/Screens/ProductsScreen';
 import { StatusBar } from 'expo-status-bar';
+import ProductDetailsScreen from './src/Screens/ProductDetailsScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ProductsScreen />
+      {/* <ProductsScreen /> */}
+      <ProductDetailsScreen />
       <StatusBar style='auto' />
     </View>
   );
