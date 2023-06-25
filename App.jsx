f8c0f602e6f02dcb554b477771a4287a26925e14
sourav_allmysons <souravmajumdar.dev@gmail.com>
2023-06-25 20:49:17 +0530

    Adding three.js in our code and compleaete the Shoue 3d view

--- App.jsx
+++ App.jsx
@@ -1,13 +1,19 @@
+import { LogBox } from 'react-native';
+LogBox.ignoreLogs([`Scripts "build/three.js"`]);
 import { Provider } from 'react-redux';
 import Navigation from './src/navigation';
 import { store } from './src/store';
 import { StatusBar } from 'expo-status-bar';
+import Three from './src/screens/Three';
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <Navigation />
-      <StatusBar style='auto' />
-    </Provider>
+    // <Provider store={store}>
+    //   <Navigation />
+    //   <StatusBar style='auto' />
+    // </Provider>
+    <>
+      <Three />
+    </>
   );
 }
